a4506f579c113543670543c6b70c3037240747b7
Mark Goodrich <mgoodrich@pih.org>
2011-05-10 18:38:24 +0000

    Improved handling of "other" option

--- web/module/portlets/addressLayout.jsp
+++ web/module/portlets/addressLayout.jsp
@@ -13,14 +13,18 @@
 	var other = "<spring:message code="addresshierarchy.other"/>";
 	
 	$j(document).ready(function(){
-		// initialize all the address field (if necessary)
+
+		// initialize all the address field (if necessary) by updating the options in the relevant select list
+		// note that we need to reference the select lists by name (as opposed to class) because there may be multiple
+		// instances of the address portlet (and therefore multiple addresses) on a single page
+		// note that we build the search string for each hierarchy level by concatenating the values of the previous levels
 		<c:forEach var="hierarchyLevel" items="${hierarchyLevels}" varStatus="i">
 			<spring:bind path="${hierarchyLevel.addressField.name}">
 				<c:if test="${not empty status.value || i.count == 1}">
 					updateOptions($j('select[name=${status.expression}]'), "${searchString}", "${status.value}");  // use double quotes here so as not conflict with ' in location names
 					<c:set var="searchString">${searchString}${fn:length(searchString) > 0 ? '|' : ''}${status.value}</c:set>
 				</c:if>
-				</spring:bind>
+			</spring:bind>
 		</c:forEach>
  	});
 </script>
@@ -32,8 +36,9 @@
 		<c:forEach var="hierarchyLevel" items="${hierarchyLevels}" varStatus="i">
 			<tr>
 				<td><spring:message code="${model.layoutTemplate.nameMappings[hierarchyLevel.addressField.name]}"/></td>
-				<td><spring:bind path="${hierarchyLevel.addressField.name}">
-					<select type="text" name="${status.expression}" class="${hierarchyLevel.addressField.name}" 
+				<spring:bind path="${hierarchyLevel.addressField.name}">
+				<td>
+					<select type="text"  style="display:none" name="${status.expression}" class="${hierarchyLevel.addressField.name}" 
 					<c:if test="${i.count < fn:length(hierarchyLevels)}">
 							onChange="handleAddressFieldChange($j(this), $j(this).closest('.address').find('.${hierarchyLevels[i.count].addressField.name}'));"
 					</c:if> 
@@ -41,8 +46,9 @@
 							onChange="handleAddressFieldChange($j(this), '');"
 					</c:if>
 					/>
-				</spring:bind></td>
-				<td><input type="text" style="display:none"/></td>
+				</td>
+				<td><input type="text" style="display:none" value="${status.value}"/></td>
+				</spring:bind>
 			</tr>
 		</c:forEach>
 	</table>
--- web/module/resources/addressHierarchy.js
+++ web/module/resources/addressHierarchy.js
@@ -1,76 +1,53 @@
 
 var $j = jQuery;	
 
+// the default handler that is called when a change event occurs on any of the address field selection lists
 function handleAddressFieldChange(changedField, fieldToUpdate) {
-		
-	// if it's been a switch to "other"
+	
+	// if it's been a switch to "other", hand off to the handleSelectOther function
 	if(changedField.val() == '--other--') {
-		handleAddressFieldChangeToOther(changedField);
+		handleSelectOther(changedField, true);
 		return;
 	}
 	
-	// if it's a switch with no field to update (ie, the last level in the hierarchy)
+	// if it's a switch with no field to update (ie, the last level in the hierarchy), just make sure we've switch back to 
+	// a selection list (to handle the case where we are switching out of the "other" state)
 	if(fieldToUpdate == '') {
-		// find the associated free text field
-		var textField = changedField.closest('tr').find(':text');
-
-		// hide the text field and reset the value
-		textField.hide();
-		textField.val('');
-		
-		if(changedField.attr('name') == '') {
-			// set the name of the selector field to the name of the free text field
-			changedField.attr('name',textField.attr('name'));
-			// clear the name of the selector
-			textField.attr('name','');
-		}
-		
+		switchToSelectList(changedField);
 		return;
 	}
 	
-	// handle the main case of updating the possible addresses
 	var searchString = '';
 
-	// we need to iterate through all the address hiearchy levels from top to bottom
+	// grab a reference to the overall address associated with the fields 
+	// (we can't directly reference the address because there may be multiple addresses on a single page)
+	var address = fieldToUpdate.closest('.address');
+
+	// handle the main case of updating the possible addresses
+	// we need to iterate through all the address hierarchy levels from top to bottom...
 	// for all levels *above* the next element, we need to build a search string in the 
 	// format "UNITED STATES|MASSACHUSETTS|SUFFOLK";
 	// then the element and all levels below it need to be emptied;
-	var reachedLevelToUpdate = false;
-	var reachedLevelToClearTextField = false;
 	
-	$j.each(addressHierarchyLevels, function (i, entry) {		
-		if (fieldToUpdate.attr('class') == entry) { 
-			reachedLevelToUpdate = true; 
-		}
-		if (changedField.attr('class') == entry) {
-			reachedLevelToClearTextField = true;
-		}
+	var changedFieldIndex = findIndexOfLevel(changedField.attr('class'));
+	
+	$j.each(addressHierarchyLevels, function (i, level) {		
 		
-		if (reachedLevelToUpdate == false) {
-			// build the search string	
-			searchString = searchString + fieldToUpdate.closest('.address').find('.' + entry).val() + "|";
+		// build the search string by concatenating the value of all fields above (and including) the field that changed
+		if (i <= changedFieldIndex ) {
+			searchString = searchString + address.find('.' + level).val() + "|";
 		}
+		// empty the other entries
 		else {
-			// empty the other entries
-			fieldToUpdate.closest('.address').find('.' + entry).empty(); 
+			var selectList = address.find('.' + level);		
+			selectList.empty();
+			selectList.append($j(document.createElement('option')).attr('value','').text(''));
+			selectList.hide();
 		}
 		
-		if (reachedLevelToClearTextField) {
-			// find the selector associated with this field
-			var selector = changedField.closest('.address').find('.' + entry);
-			// find the associated free text field
-			var textField = selector.closest('tr').find(':text');
-	
-			// hide the text field and reset the value
-			textField.hide();
-			textField.val('');
-	
-			if(selector.attr('name') == '') {
-				// set the name of the selector field to the name of the free text field
-				selector.attr('name',textField.attr('name'));
-				// clear the name of the selector
-				textField.attr('name','');
-			}
+		// clear the text input field of the changed field and all fields below it in the hierarchy
+		if (i >= changedFieldIndex) {
+			switchToSelectList(address.find('.' + level));
 		}
 	});
 
@@ -78,59 +55,113 @@ function handleAddressFieldChange(changedField, fieldToUpdate) {
 	if (searchString != null) {
 		searchString = searchString.slice(0,-1);
 	}
-		
-	updateOptions(fieldToUpdate, searchString);
+	
+	if (searchString != '') {
+		// update the options of the field to update
+		updateOptions(fieldToUpdate, searchString, '');
+	}
 }
 
 function updateOptions(fieldToUpdate, searchString, value) {	
-	// TODO: handle the "other" options
 	
 	// do the JSON call and add the appropriate elements
 	$j.getJSON(pageContext + '/module/addresshierarchy/ajax/getChildAddressHierarchyEntries.form',
 			{ 'searchString': searchString },
 			function (data) {
-				fieldToUpdate.append($j(document.createElement('option')).text('--'));
+				var foundCurrentValue;
+				
+				fieldToUpdate.empty();
+				fieldToUpdate.append($j(document.createElement('option')).attr('value','').text(''));
 				$j.each(data, function(i, entry) {
 					var option = $j(document.createElement('option')).attr('value', entry.name).text(entry.name);
 					if (entry.name == value) {
 						option.attr('selected',true);
+						foundCurrentValue = true;
 					}
 					fieldToUpdate.append(option);
 				});
 				fieldToUpdate.append($j(document.createElement('option')).attr('value', '--other--').text(other));
+				
+				// if we haven't found a match against the current value of the field, switch tto the "Other" case
+				if (!foundCurrentValue && value !='') {		
+					fieldToUpdate.val('--other--')
+					handleSelectOther(fieldToUpdate, false);
+				}
+				
+				// show the select list
+				fieldToUpdate.show();
 			}
 		);
 }
 
-function handleAddressFieldChangeToOther(field) {
-	
-	var reachedLevelToSetToOther = false;
-	
+// called from handleAddressFieldChange to handle the case when a user selects "Other" from the list of options
+// also used to switch fields into the "Other" state upon initialization as required
+function handleSelectOther(field, emptyTextInput) {
+
+	var fieldLevelIndex = findIndexOfLevel(field.attr('class'));
+	var address = field.closest('.address');
+
 	// iterate through all the address hierarchy levels
-	$j.each(addressHierarchyLevels, function (i, entry) {		
-		if (field.attr('class') == entry) { 
-			// we only want to operate on the level associated with the current field and all subsequent level
-			reachedLevelToSetToOther = true; 
-		}
-		if (reachedLevelToSetToOther == true) {
-			// find the selector associated with this field
-			var selector = field.closest('.address').find('.' + entry);
-			// find the associated free text field
-			var textField = selector.closest('tr').find(':text');
-			// empty the selector (as long as it's not the top level field)
-			if (entry != field.attr('class')) {
-				selector.empty();
-			}
-			// show the associated free text field
-			textField.show();
+	$j.each(addressHierarchyLevels, function (i, level) {		
+		// we want to skip all the hierarchy levels before the level of the address field we are operating on
+		if (i >= fieldLevelIndex){
 			
-			if(selector.attr('name') != '') {
-				// set the name of the free text field to the name of the selector
-				textField.attr('name',selector.attr('name'));
-				// clear the name of the selector
-				selector.attr('name','');
+			var selectList = address.find('.' + level);
+			
+			// empty all select lists of hierarchy levels below the field that was set to "other"
+			if (i > fieldLevelIndex) {
+				selectList.empty();
+				selectList.hide();
+				//selectList.append($j(document.createElement('option')).attr('value','--other--').text(other));
 			}
+			
+			// switch to using the text input instead of the select list
+			switchToTextInput(selectList, emptyTextInput);
 		}
 	});
+}
+
+// for the specified field, switch to using the text input instead of the select list
+function switchToTextInput(selectList, emptyTextInput) {
+	// find the associated free text field
+	var textInput = selectList.closest('tr').find(':text');
 	
+	// show the associated free text field
+	textInput.show();
+	if(emptyTextInput == true) {
+		textInput.val('');
+	}
+	
+	if(selectList.attr('name') != '') {
+		// set the name of the free text field to the name of the selector
+		textInput.attr('name',selectList.attr('name'));
+		// clear the name of the selector
+		selectList.attr('name','');
+	}
+}
+
+// for the specified field, switch from using the text input to the select list
+function switchToSelectList(selectList)  {
+	// find the associated free text field
+	var textInput = selectList.closest('tr').find(':text');
+
+	// hide the text field and reset the value
+	textInput.hide();
+	textInput.val('');
+
+	if(selectList.attr('name') == '') {
+		// set the name of the selector field to the name of the free text field
+		selectList.attr('name',textInput.attr('name'));
+		// clear the name of the selector
+		textInput.attr('name','');
+	}
+}
+
+// find the numerical index of a level within the address hierarchy
+function findIndexOfLevel(level) {
+	for(var i=0; i<addressHierarchyLevels.length; i++){ 
+	       if(addressHierarchyLevels[i] == level) {
+	            return i;
+	       }
+	}
 }
