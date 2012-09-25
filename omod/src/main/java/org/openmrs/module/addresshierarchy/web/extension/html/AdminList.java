4d5adf064e9298a8f3e163289b2b355b3c6aa1b3
Mark Goodrich <mgoodrich@pih.org>
2012-09-25 13:45:01 +0000

    ADDR-46: Move extension.html from api component to web component

--- omod/src/main/java/org/openmrs/module/addresshierarchy/web/extension/html/AdminList.java
+++ omod/src/main/java/org/openmrs/module/addresshierarchy/web/extension/html/AdminList.java
@@ -11,7 +11,7 @@
  *
  * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
  */
-package org.openmrs.module.addresshierarchy.extension.html;
+package org.openmrs.module.addresshierarchy.web.extension.html;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
