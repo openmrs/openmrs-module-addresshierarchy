031017e6296335030a2aeda4cb674e13e69f8adb
krishnanspace <34261449+krishnanspace@users.noreply.github.com>
2019-04-26 20:33:42 +0530

    Krishnan | Added the fields to allow address7-address15 in Address Hierarchy selection (#24)

--- api/src/main/java/org/openmrs/module/addresshierarchy/AddressField.java
+++ api/src/main/java/org/openmrs/module/addresshierarchy/AddressField.java
@@ -17,7 +17,9 @@ public enum AddressField {
 	
 	ADDRESS_1("address1"), ADDRESS_2("address2"), ADDRESS_3("address3"), NEIGHBORHOOD_CELL("neighborhoodCell"), 
 	ADDRESS_4("address4"), TOWNSHIP_DIVISION("townshipDivision"), ADDRESS_5("address5"), SUBREGION("subregion"), 
-	ADDRESS_6("address6"), REGION("region"), CITY_VILLAGE("cityVillage"), COUNTY_DISTRICT("countyDistrict"), 
+	ADDRESS_6("address6"),ADDRESS_7("address7"),ADDRESS_8("address8"),ADDRESS_9("address9"),ADDRESS_10("address10"),
+	ADDRESS_11("address11"),ADDRESS_12("address12"),ADDRESS_13("address13"),ADDRESS_14("address14"),ADDRESS_15("address15"),
+	REGION("region"), CITY_VILLAGE("cityVillage"), COUNTY_DISTRICT("countyDistrict"), 
 	STATE_PROVINCE("stateProvince"), COUNTRY("country"), POSTAL_CODE("postalCode"), LONGITUDE("longitude"),
     LATITUDE("latitude");
 	
